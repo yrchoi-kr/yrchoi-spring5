c40e55c1bf36b5aafd9582bd9d4ba4f5d4386316
KimYoungJea <admin@DESKTOP-5V6RT96>
2021-06-15 09:45:46 +0900

    신규커밋

--- src/main/webapp/resources/home/js/main.js
+++ src/main/webapp/resources/home/js/main.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
     $('.to_top a').smoothScroll();
-
-    // 모바일용 메뉴 보이기/숨기기 액션처리
-	$('.openMOgnb').click(function(){
-		// alert('메뉴보이기 액션클릭');
+    	// 모바일용 메뉴 보이기/숨기기
+	$('.openMOgnb').click(function() {
+		// alert('메뉴보이기 액션 클릭')
 		// $('.header_cont').css('display','block');
 		$('.header_cont').slideDown("slow");
 		$('#header').addClass('on');
 	});
 	$('.closePop').click(function(){
-		// $('.header_cont').css('display','none');
+		// $('.heaser_cont').css('display','none');
 		$('.header_cont').slideUp("fast");
 		$('#header').removeClass('on');
 	});
 });
+
 
 // PC용 서브메뉴가 사라지는 액션(아래)
 var isOver1 = false; // 대메뉴1이 false 일때 서브메뉴가 사라지는 조건
--- src/main/webapp/resources/test/json.js
+++ src/main/webapp/resources/test/json.js
@@ -1,122 +1,122 @@
 var jsonData = [
     {
-      "_id": "609e126dd88c8a20eabde3c7",
-      "age": 26,
-      "eyeColor": "green",
-      "name": "Atkins Palmer"
-    },
-    {
-      "_id": "609e126dd2c4e68b72c8b01f",
-      "age": 36,
+      "_id": "609e12790f6e2567180dbae9",
+      "age": 24,
       "eyeColor": "brown",
-      "name": "Emily Trujillo"
+      "name": "Middleton Goff"
     },
     {
-      "_id": "609e126ddd5e8186c1312b8a",
-      "age": 22,
-      "eyeColor": "green",
-      "name": "Ursula Weaver"
-    },
-    {
-      "_id": "609e126d899f69db91d6b1ca",
-      "age": 34,
-      "eyeColor": "blue",
-      "name": "Day Witt"
-    },
-    {
-      "_id": "609e126db8ca82a0cd0847d5",
-      "age": 37,
-      "eyeColor": "blue",
-      "name": "Pamela Knowles"
-    },
-    {
-      "_id": "609e126df430ec4e3145b1e1",
-      "age": 40,
-      "eyeColor": "brown",
-      "name": "Irma Vasquez"
-    },
-    {
-      "_id": "609e126d4e12c9fef41c19ec",
-      "age": 39,
-      "eyeColor": "brown",
-      "name": "Guy Sheppard"
-    },
-    {
-      "_id": "609e126de4428294a6a28950",
-      "age": 37,
-      "eyeColor": "green",
-      "name": "Bray Cross"
-    },
-    {
-      "_id": "609e126d476bd2c18c574199",
-      "age": 20,
-      "eyeColor": "brown",
-      "name": "Holman Cummings"
-    },
-    {
-      "_id": "609e126d5d514a0df2490b66",
+      "_id": "609e127950b0cb80402a04ec",
       "age": 25,
       "eyeColor": "blue",
-      "name": "Claudia Miles"
+      "name": "Tammi Simmons"
     },
     {
-      "_id": "609e126d328eb801053c2f86",
-      "age": 39,
-      "eyeColor": "brown",
-      "name": "Vasquez Gregory"
-    },
-    {
-      "_id": "609e126d139a7cab43b7d6bc",
+      "_id": "609e127996ab2af7d3d03e69",
       "age": 29,
-      "eyeColor": "green",
-      "name": "Scott Slater"
-    },
-    {
-      "_id": "609e126d7d3cc1f74a2eebaf",
-      "age": 27,
       "eyeColor": "brown",
-      "name": "Rosario Ayala"
+      "name": "Cohen Wong"
     },
     {
-      "_id": "609e126d8979530b5168cf76",
-      "age": 26,
-      "eyeColor": "blue",
-      "name": "Beth Shannon"
-    },
-    {
-      "_id": "609e126dabd336ff8dde21a0",
-      "age": 38,
-      "eyeColor": "brown",
-      "name": "Wendy Dodson"
-    },
-    {
-      "_id": "609e126dcf9503e8f6b8e33b",
-      "age": 39,
-      "eyeColor": "brown",
-      "name": "Dorthy Fuller"
-    },
-    {
-      "_id": "609e126d295254d4db97e7a9",
+      "_id": "609e1279ab0fa31fe8e11369",
       "age": 32,
-      "eyeColor": "green",
-      "name": "Carly Potts"
+      "eyeColor": "blue",
+      "name": "Allen Rosa"
     },
     {
-      "_id": "609e126dc1ac917cf6826977",
-      "age": 22,
-      "eyeColor": "green",
-      "name": "English Juarez"
+      "_id": "609e1279519533482b153b36",
+      "age": 20,
+      "eyeColor": "blue",
+      "name": "Lowery Ryan"
     },
     {
-      "_id": "609e126d4f179e9778947a59",
+      "_id": "609e1279a420d2004c041bc4",
+      "age": 28,
+      "eyeColor": "green",
+      "name": "Mindy Ortega"
+    },
+    {
+      "_id": "609e12793cb3e9f4f1c89b69",
+      "age": 38,
+      "eyeColor": "blue",
+      "name": "Russell Mcknight"
+    },
+    {
+      "_id": "609e1279a00f59b4ad79f2fb",
+      "age": 25,
+      "eyeColor": "blue",
+      "name": "Hobbs Sullivan"
+    },
+    {
+      "_id": "609e127951d503303c8c9869",
+      "age": 25,
+      "eyeColor": "green",
+      "name": "Tara Farley"
+    },
+    {
+      "_id": "609e12794e9c738e4187c769",
       "age": 40,
       "eyeColor": "green",
-      "name": "Lyons Glass"
+      "name": "Goodwin Harding"
     },
     {
-      "_id": "609e126d7513f14f0cd985f3",
-      "age": 30,
+      "_id": "609e12796e930f8cfef79517",
+      "age": 23,
+      "eyeColor": "green",
+      "name": "Peters Ewing"
+    },
+    {
+      "_id": "609e1279170cbf54944544f1",
+      "age": 36,
+      "eyeColor": "green",
+      "name": "Pollard Black"
+    },
+    {
+      "_id": "609e1279b95e899a1ad231e6",
+      "age": 23,
+      "eyeColor": "green",
+      "name": "Paige Franklin"
+    },
+    {
+      "_id": "609e127921ed1ee59c702f7f",
+      "age": 40,
       "eyeColor": "brown",
-      "name": "Harvey Miller"
+      "name": "Winters Williamson"
+    },
+    {
+      "_id": "609e1279f553af006d7245ac",
+      "age": 25,
+      "eyeColor": "brown",
+      "name": "Roberta Blackwell"
+    },
+    {
+      "_id": "609e12799229316bbbcf5ef6",
+      "age": 34,
+      "eyeColor": "brown",
+      "name": "Barber Todd"
+    },
+    {
+      "_id": "609e12793b3eb760cf02cca6",
+      "age": 26,
+      "eyeColor": "green",
+      "name": "Viola Schwartz"
+    },
+    {
+      "_id": "609e127932717dcf29ce7392",
+      "age": 32,
+      "eyeColor": "brown",
+      "name": "Vera Austin"
+    },
+    {
+      "_id": "609e127925926a87e4f434a5",
+      "age": 23,
+      "eyeColor": "blue",
+      "name": "Mercado Holder"
+    },
+    {
+      "_id": "609e1279088dc4f80386325f",
+      "age": 27,
+      "eyeColor": "green",
+      "name": "Ashley Burke"
     }
   ]
